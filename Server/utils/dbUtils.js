898c80bac19e7eb144902aba2ddbd3ede98580af
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-09-03 15:26:42 +0200

    utils_db drop and create

---
+++ Server/utils/dbUtils.js
@@ -0,0 +1,43 @@
+const { knex } = require('./db');
+
+module.exports.up = function () {
+  // Create referenced table before referencing table.
+  return knex.schema
+    .createTable('users', function (users) {
+      users.increments('userID').primary();
+      users.string('name');
+      users.string('password');
+    })
+    .createTable('samples', function (samples) {
+      samples.increments('samplesID').primary();
+      samples.string('meassurement');
+      samples
+        .integer('userID')
+        .references('userID')
+        .inTable('users')
+        .notNull()
+        .onDelete('cascade');
+    })
+    .then(() => console.log('table created'))
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+};
+
+module.exports.down = function () {
+  // Reverse order here to prevent error.
+  return knex.schema
+    .dropTable('users')
+    .dropTable('samples')
+    .then(() => console.log('tables droped'))
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+};
