7fd7a6897b295b625e9d042cad832e13156c4245
Juan-Avila92 <juan.avila@3dves.com>
2020-09-19 10:25:27 -0500

    routes

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -1,7 +1,10 @@
 const userApi = require('./usersRoutes');
+const samples = require('./sampleRoutes');
 
-const routes = function usersApi(app) {
+const routes = function splApi(app) {
   app.use('/api/users', userApi);
+  app.use('/api/samples', samples);
 };
 
 module.exports = routes;
+
