2fc20207ac3c96802a87278c92da8e532205a650
Juan-Avila92 <juan.avila@3dves.com>
2020-09-19 10:24:45 -0500

    index config

--- Server/config/index.js
+++ Server/config/index.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 
+
 const config = {
   dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 3000,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST,
-  DB_NAME: process.env.DB_NAME,
-  DB_PORT: process.env.DB_PORT,
-  CONECTION_STRING: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  port: 3000,
+  DB_USER: 'postgres',
+  DB_PASSWORD: 'postgres',
+  DB_HOST: '127.0.0.1',
+  DB_NAME: 'spl_database',
+  DB_PORT: 5432,
 };
-
 module.exports = { config };
 
 //Created by Santiago Rey
