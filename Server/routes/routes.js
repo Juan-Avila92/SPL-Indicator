3921ced32e14edcb49ac9f75b335e4475303f29e
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-09-19 17:23:52 +0200

    samples api

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -1,7 +1,9 @@
 const userApi = require('./usersRoutes');
 
-const routes = function usersApi(app) {
+const routes = function appApis(app) {
   app.use('/api/users', userApi);
+  //TODO @Juan-Avila92 userApi need to be change for samples api
+  app.use('/api/samples', userApi);
 };
 
 module.exports = routes;
