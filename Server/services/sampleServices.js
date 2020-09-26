594ccb4b491483c73cfcb8b720e46b3e2261dc06
Juan-Avila92 <juan.avila@3dves.com>
2020-09-26 10:45:03 -0500

    sample changes

--- Server/services/sampleServices.js
+++ Server/services/sampleServices.js
@@ -1,12 +1,15 @@
-// posible use for other propouses
-//const { samplesMock } = require('../../utils/mocks/samplesMock');
 var {
     createNewSample,
+    getAllSamples,
   } = require('./sampleQueries');
   
   class SamplesServices {
     async createNewSample(body) {
       return createNewSample(body) || [];
+    }
+
+    async getAllSamples() {
+      return getAllSamples() || [];
     }
   }
   module.exports = SamplesServices;
